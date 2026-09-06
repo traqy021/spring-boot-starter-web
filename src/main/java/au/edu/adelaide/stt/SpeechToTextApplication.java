76ee75c51e94f8214f6ba5745d0a8d96c12519e7
noahtran04 <noah.tran@student.adelaide.edu.au>
2026-09-07 00:22:48 +0930

    Initial Spring Boot project

---
+++ src/main/java/au/edu/adelaide/stt/SpeechToTextApplication.java
@@ -0,0 +1,13 @@
+package au.edu.adelaide.stt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechToTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechToTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/edu/adelaide/stt/SpeechToTextApplicationTests.java
@@ -0,0 +1,13 @@
+package au.edu.adelaide.stt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechToTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
