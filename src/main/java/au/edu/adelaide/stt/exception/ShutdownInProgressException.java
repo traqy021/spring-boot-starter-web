39b7184486661cb4e4b0dbff18c65a1f58f5cc5d
noahtran04 <noah.tran@student.adelaide.edu.au>
2026-09-13 22:55:53 +0930

    Changing the shutdown script

--- src/main/java/au/edu/adelaide/stt/exception/ShutdownInProgressException.java
+++ src/main/java/au/edu/adelaide/stt/exception/ShutdownInProgressException.java
@@ -6,7 +6,7 @@ public class ShutdownInProgressException
     public ShutdownInProgressException() {
 
         super(
-            "Shutting down is already in progress."
+        	"Graceful shutdown is already in progress."
         );
     }
 }
